a99a93a8c2fd9c1768050ea9317030bbfeb35eba
KyawThetKhing <ktkhaing47@gmail.com>
2025-03-31 11:17:35 +0700

    feat: project setup

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+    })
+)
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello World! Updated')
+})
+
+module.exports = app
